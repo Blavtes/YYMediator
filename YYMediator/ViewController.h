a523b9aaca1cbb8291530e389df38ff110d8e222
Blavtes <Blavtes@yangyong.local>
2017-11-06 17:22:48 +0800

    init

---
+++ YYMediator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYMediator
+//
+//  Created by Blavtes on 2017/11/6.
+//  Copyright © 2017年 Blavtes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
